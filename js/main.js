3e005c6619d08e19cc92e54eba0d56bd2f95998e
MilkBefourCereal <112642195+MilkBefourCereal@users.noreply.github.com>
2022-10-11 08:47:44 -0600

    added js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,81 @@
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
+let sUpPw = document.getElementById("signUpPassword");
+let sUpUser = document.getElementById("signUpUser");
+let confirmPw = document.getElementById("confirmPassword");
+let sInPw = document.getElementById("signInPassword");
+let sInUser = document.getElementById("signInUser");
+
+let signUpInfo = loadSignUpInfo();
 
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
-function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+function signUpHandler() { 
+  let index = findbyUser(sUpUser.value);
+  if (sUpPw.value === "" || confirmPw.value === "") {
+    alert("Missing Password");
+    return ;
+  } else if (sUpUser.value === "") {
+    alert("Missing Username");
+    return ;
+  } 
+  if (confirmPw.value !== sUpPw.value) {
+   alert("Passwords dont match");
+   return ;
+  } else {
+    if (index === -1){
+      signUpInfo.push(newSignUp(sUpUser.value, sUpPw.value, confirmPw.value))
+    } else {
+      alert("This Username has been used");
+    }
+    saveInfo();
+  }
+ 
 }
 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+  if (sInPw.value === "" || sInUser.value === "") {
+    alert("Missing a Input");
+  } else {
+  for (let i = 0; i < signUpInfo.length; i++) {
+    if (signUpInfo[i].Password === sInPw.value && signUpInfo[i].Username === sInUser.value) {
+      alert("Sign In Successful");
+      return ;
+    }
+    } 
+    alert("Sign In Unsuccessful")
+}
+}
+
+
+function loadSignUpInfo() {
+  let infoStr = localStorage.getItem('signUpInfo')
+  return JSON.parse(infoStr) ?? [];
+}
+
+function saveInfo() {
+  localStorage.setItem('signUpInfo', JSON.stringify(signUpInfo))
+}
+
+function newSignUp(Username, Password, cPassword) {
+  alert("Sign up Complete")
+  return {
+    Username: Username,
+    Password: Password, 
+    cPassword: cPassword
+  };
+}
+
+function findbyUser(Username) {
+  for (let i = 0; i < signUpInfo.length; i++) {
+    if (signUpInfo[i].Username === Username) {
+      return 1;
+    }
+  }
+  return -1;
 }
